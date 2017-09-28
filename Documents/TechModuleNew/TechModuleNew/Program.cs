07e8c70704cac7c91469a17a2c8c1f2b125b50d3
jefri931 <daniel.stoynev@icloud.com>
2017-09-28 20:57:23 +0300

    TechModuleNew

---
+++ Documents/TechModuleNew/TechModuleNew/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechModuleNew
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
